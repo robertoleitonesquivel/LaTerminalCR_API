fe0c7986f44b82abf2d139fa72c649b004fbde4a
robertoleitonesquivel <rleiton22@gmail.com>
2024-05-09 18:46:35 -0600

    cambios

--- PruebaTecnicaAPI/Program.cs
+++ PruebaTecnicaAPI/Program.cs
@@ -19,22 +19,29 @@ builder.Services.AddScoped<ICommon, Common>();
 builder.Services.AddScoped<ICitiesService, CitiesService>();
 builder.Services.AddScoped<ITicketsService, TicketsService>();
 builder.Services.AddHttpClient();
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("TerminalCR", builder =>
+	{
+		builder.AllowAnyOrigin()
+			   .AllowAnyMethod()
+			   .AllowAnyHeader();
+	});
+});
 
 #endregion
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("TerminalCR");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
